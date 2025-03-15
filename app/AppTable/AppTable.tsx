09b6d8f4f0758a607b9ad7804ef1beb321341571
fernandes-draw <fernandes_draw@hotmail.com>
2025-03-14 23:33:45 -0300

    Create navbar and header product

---
+++ app/AppTable/AppTable.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
+export default function AppTable() {
+  return (
+    <Card className='mt-12  flex flex-col shadow-none  poppins border-none '>
+      <CardHeader className='flex justify-between'>
+        <div className='flex justify-between items-center'>
+          <div className=''>
+            <CardTitle className='font-bold text-[23px] '>Products</CardTitle>
+            <p className='text-sm text-slate-600'>34 products</p>
+          </div>
+          <Button>Add Product</Button>
+        </div>
+      </CardHeader>
+
+      <CardContent></CardContent>
+    </Card>
+  );
+}
